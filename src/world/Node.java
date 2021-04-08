6bb882a01f3880fff5dc9a6e2c24257837765c61
Adrian Cosic <e12025964@tuwien.student.ac.at>
2021-04-08 22:42:25 +0200

    nodes, nodelist etc.

--- src/world/Node.java
+++ src/world/Node.java
@@ -1,4 +1,29 @@
 package world;
 
 public class Node {
+
+    Node north;
+    Node south;
+    Node west;
+    Node east;
+    Node next;
+    Area area;
+    int x;
+    int y;
+
+//TODO: constructor braucht: event, coord., nachbarn
+    public Node(int x, i
+        this.x = x;
+        this.y = y;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public void setNext(Node node)
+    {
+        next = node;
+    }
 }
---
+++ src/world/nodeList.java
@@ -0,0 +1,20 @@
+package world;
+
+public class nodeList {
+
+    private String name;
+    private Node head;
+    private Node last;
+
+    public nodeList(String name) {
+        this.name = name;
+    }
+/*
+    public void add(Node node) {
+        Node element = node;
+        if (head =
+
+    }
+    */
+
+}
